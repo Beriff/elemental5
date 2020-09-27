ce4e0eea655fb66b88d10cd7ea2c02920939bd7b
Beriff <dofite.yes@gmail.com>
2020-09-27 19:04:41 +0700

    Add web server

---
+++ server.py
@@ -0,0 +1,38 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import urllib.parse as parse
+import json
+
+elements = "<html><body>Hello</body></html>"
+fields = None
+
+
+class Processor(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('content-type','text/html')
+        self.end_headers()
+
+
+        self.wfile.write(str(elements).encode())
+
+    def do_POST(self):
+        global elements
+
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        elements = str(post_data).replace('b', '').replace("'", '')
+
+        self.send_response(200)
+        self.send_header('content-type','text/html')
+        self.end_headers()
+
+server = HTTPServer(('localhost', 81), Processor)
+server.serve_forever()
+
+if __name__ == '__main__':
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
