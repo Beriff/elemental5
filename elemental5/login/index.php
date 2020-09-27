85fce50fc42c4630679f98d953cb314e449fa4b6
Beriff <dofite.yes@gmail.com>
2020-09-27 16:59:34 +0700

    ➕ Add redirect if user is logined

--- elemental5/login/index.php
+++ elemental5/login/index.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
+
+if(isset($_COOKIE["user"])) {
+    echo "<script type='text/javascript'>location.href = '../';</script>";
+}
+
 $state = "Login";
 
 $servername = "localhost";
