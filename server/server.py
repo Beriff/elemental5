e62a634f3e87dc2068b39d0a87f388d8dfb6ac7d
Beriff <dofite.yes@gmail.com>
2020-09-27 20:42:16 +0700

    🔧 Fix json read/write

--- server/server.py
+++ server/server.py
@@ -2,11 +2,13 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import urllib.parse as parse
 import json
 
-with open('elements.json') as f:
-    data = json.load(f)
+f = open('elements.json', mode='r+')
+data = json.load(f)
+f.close()
 
-elements = data
+elements = dict(data)
 fields = None
+
 
 
 class Processor(BaseHTTPRequestHandler):
@@ -20,10 +22,30 @@ class Processor(BaseHTTPRequestHandler):
 
     def do_POST(self):
         global elements
+        f2 = open('elements.json', mode='w+')
 
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
-        elements = str(post_data).replace('b', '').replace("'", '')
+
+        print('received data')
+
+        save = eval(str(post_data).replace('b', '').replace("'", ''))
+
+        print(save)
+
+        updatedElements = {**elements, **save}
+
+        print('merged dicts')
+
+        f2.write(json.dumps(updatedElements))
+
+        print('written to file')
+
+        elements = dict(updatedElements)
+        f.close()
+
+        print('closed')
+
 
         self.send_response(200)
         self.send_header('content-type','text/html')
